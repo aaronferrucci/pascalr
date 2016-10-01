3aa9b68a0981850ca79b606148ad4f842bd302d3
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2016-10-01 15:00:40 -0700

    Add shiny app

---
+++ app.R
@@ -0,0 +1,72 @@
+library(shiny)
+library(numbers)
+source("main.R")
+
+# Fill functions. All must return integer.
+mod2 <- function(v, n) ifelse(v %% 2 == 0, 1, 0)
+mod3 <- function(v, n) ifelse(v %% 3 == 0, 1, 0)
+modn <- function(v, n) {ifelse(n == 0, 0, ifelse(v %% n == 0, 1, 0))}
+prime <- function(v, n) ifelse(n == 0, 0, ifelse(isPrime(n) && modn(v, n), 1, 0))
+value <- function(v, n) v
+color_table <- c(
+  "value mod 2 == 0" = "mod2",
+  "value mod 3 == 0" = "mod3",
+  "value mod n == 0" = "modn",
+  "n is prime and value mod n == 0" = "prime",
+  "value" = "value"
+)
+function_table <- c(
+  "mod2" = mod2,
+  "mod3" = mod3,
+  "modn" = modn,
+  "prime" = prime,
+  "value" = value
+)
+
+desc_table <- c(
+  "mod2" = "Even numbered values are colored",
+  "mod3" = "Values divisible by 3 are colored",
+  "modn" = "Values disivible by their row number are colored",
+  "prime" = "Values divisible by their row number, in prime numbered rows are colored",
+  "value" = "Color gradient according to value"
+)
+
+ui <- shinyUI(fluidPage(
+   
+   # Application title
+   titlePanel("Pascal's Triangle"),
+   
+   sidebarLayout(
+      sidebarPanel(
+        selectInput('colorfn', 'color', color_table, "mod2"),
+        checkboxInput('show_values', 'Show Values', TRUE),
+        htmlOutput('text')
+      ),
+      # Show a plot of the generated distribution
+      mainPanel(
+         plotOutput("distPlot")
+      )
+   )
+))
+
+server <- shinyServer(function(input, output) {
+   output$text <- renderText({HTML(paste(
+     "Each cell's value is the sum of the cells just above it",
+     paste0("The triangle is arranged as n rows numbered from 0 to ", max_row),
+     paste0('<br/>', desc_table[[input$colorfn]]),
+     sep = '<br/>'
+   ))})
+   output$distPlot <- renderPlot({
+     # choose a function
+     fn <- function_table[[input$colorfn]]
+     
+     # apply the function to fill value
+     h <- lapply(h, function(p) { p$fill <- fn(p$value, p$n); return(p)})
+     p <- plot(h, input$show_values)
+     print(p)
+   })
+})
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
--- main.R
+++ main.R
@@ -1,22 +1,29 @@
 library(ggplot2)
 source("utils.R")
+value_format <- function(v) {
+  return(ifelse(v > 999, "---", v))  
+}
+
+# avoid "evaluation nested too deeply":
+options(expressions=10000)
+max_row = 12
 h <- list()
 center.x <- as.integer()
 center.y <- as.integer()
 value <- as.integer()
 i <- 1
-for (n in 0:12) {
+for (n in 0:max_row) {
   for (k in 0:n) {
     center.x[i] <- k - 0.5*n
     center.y[i] <- -n * sin(pi/3)
-    # value[i] = choose(n, k)
     x <- k - 0.5*n
     y <- -n * sin(pi/3)
     value[i] = choose(n, k)
-    h[[i]] <- cbind(hexagon(c(center.x[i], center.y[i])), data.frame(n=n, k=k, value=value[i]))
+    h[[i]] <- cbind(hexagon(c(center.x[i], center.y[i])), data.frame(n=n, k=k, value=value[i], fill=0))
     i <- i + 1
   }
 }
+
 g <- ggplot() + coord_fixed(1) +
   theme(
     legend.position="none",
@@ -28,5 +35,12 @@ g <- ggplot() + coord_fixed(1) +
     axis.title.x=element_blank(),
     axis.title.y=element_blank()  
   ) +
-  lapply(h, function(p) geom_polygon(data=p, aes(x=hx, y=hy, alpha=0, fill=value %% 2), color="black")) +
-  annotate("text", x=center.x, y=center.y, label=value)
+  scale_fill_gradient(low = "gray", high = "red")
+
+plot <- function(polygons, show_values) {
+  p <- g +
+    lapply(polygons, function(p) geom_polygon(data=p, aes(x=hx, y=hy, alpha=0, fill=fill), color="black"))
+  if (show_values)
+    p <- p + annotate("text", x=center.x, y=center.y, label=value_format(value))
+  return(p)
+}
